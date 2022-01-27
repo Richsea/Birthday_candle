49311eff5788e7cf3f4009680265978d63f89ec6
Richsea <bhyeh1@gmail.com>
2022-01-28 00:08:02 +0900

    api 문제 해결중

---
+++ app/src/main/java/com/example/birthday_candle/CakeView.kt
@@ -0,0 +1,17 @@
+package com.example.birthday_candle
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+
+class CakeView : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+        //btnCakeView
+
+    }
+}
--- app/src/main/java/com/example/birthday_candle/MainActivity.kt
+++ app/src/main/java/com/example/birthday_candle/MainActivity.kt
@@ -3,12 +3,30 @@ package com.example.birthday_candle
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*;
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        btnCakeView
+        // Button action
+        //val moveButton = (Button) findViewById(R.id.activeButton)
+        /*(
+        R.id.btnCakeView.setOnClickListener({
+            val intent = Intent(this, SubActivity::class.java)
+            startActivity(intent)
+        })
+         */
+
+        /*
+        Log -> 메서드 d(tag: "Tag"(안드로이드 로그), msg: "메시지")
+         */
     }
+
+
 
     //Intent.putExtra(key, data)
 }
